b6a232954600e3d1e6efc60b0c2305f510a84442
Nehrk <1d.n3hrk@gmail.com>
2019-08-10 00:03:42 +0300

    Intital

---
+++ Assets/EnemyBase.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using DG.Tweening.Plugins.Core.PathCore;
+using UnityEngine;
+
+[RequireComponent(typeof(DOTweenPath))]
+public class EnemyBase : MonoBehaviour
+{
+    [SerializeField]
+    private Path pathComponent;
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void NewTarget()
+    {
+        float closest = float.MaxValue;
+        foreach (var target in FindObjectsOfType<Killable>())
+        {
+            var dist=Vector2.Distance(target.transform.position, transform.position);
+            if (dist < closest)
+                closest = dist;
+        }
+    }
+}
---
+++ Assets/EnemySpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    public class EnemySpawn : MonoBehaviour
+    {
+        [SerializeField] private List<Transform> _spawnPositions;
+        [SerializeField] private List<GameObject> _gameObjectsToSpawn;
+        [SerializeField] private Vector3 _spawnOffset;
+        [SerializeField] private float spawnTime = 4f;
+
+        // ReSharper disable once UnusedMember.Local
+        void Start()
+        {
+            StartCoroutine(SpawnByTimer());
+        }
+
+        // Update is called once per frame
+        private IEnumerator SpawnByTimer()
+        {
+            do
+            {
+                yield return new WaitForSeconds(spawnTime);
+
+                var objectToSpawn = _gameObjectsToSpawn[Random.Range(0, _gameObjectsToSpawn.Count)];
+                var positionToSpawn = _spawnPositions[Random.Range(0, _spawnPositions.Count)];
+                var newGo = Instantiate(objectToSpawn, positionToSpawn);
+
+                Debug.Log(newGo.name + " spawned at " + newGo.transform.position);
+            
+            } while (useGUILayout);
+        }
+    }
+}
