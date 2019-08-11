3a00f6cdae6a071be9eaf41a4da8f3defab90707
Nehrk <1d.n3hrk@gmail.com>
2019-08-11 07:56:14 +0300

    New parts & materials

---
+++ Assets/RotateScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        RotateEndless();
+    }
+
+    // Update is called once per frame
+    private void RotateEndless()
+    {
+        transform.DOBlendableLocalRotateBy(new Vector3(0, 180, 0), 1).OnComplete(RotateEndless).SetEase(Ease.Linear);
+    }
+}
