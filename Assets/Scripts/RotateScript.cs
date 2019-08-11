19fe553cf55d2b317267a2ad3724a304807623bd
Nehrk <1d.n3hrk@gmail.com>
2019-08-11 12:56:27 +0300

    Update RotateScript.cs
    fix dotween crash

--- Assets/Scripts/RotateScript.cs
+++ Assets/Scripts/RotateScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class RotateScript : MonoBehaviour
 {
+    private Tween _currentTween;
     // Start is called before the first frame update
     private void Start()
     {
         RotateEndless();
     }
 
+    private void OnDisable()
+    {
+        _currentTween.Kill();
+    }
+
     // Update is called once per frame
     private void RotateEndless()
     {
-        transform.DOBlendableLocalRotateBy(new Vector3(0, 180, 0), 1).OnComplete(RotateEndless).SetEase(Ease.Linear);
+        _currentTween = transform.DOBlendableLocalRotateBy(new Vector3(0, 180, 0), 1).OnComplete(RotateEndless).SetEase(Ease.Linear);
     }
 }
